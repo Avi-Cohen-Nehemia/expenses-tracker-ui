7c7bd4500d3dcd425cae7b0f205dee7e92b6ac1c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-21 14:02:50 +0000

    import ant design css to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import 'antd/dist/antd.css';
+import Login from './components/Login';
 import Header from './components/Header';
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Login />
+    </>
   );
 }
 
