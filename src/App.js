af02a0afddb20e7b8e626dbe23830271c95c4da4
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-22 09:49:52 +0000

    wrap signup component with Route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Container from 'react-bootstrap/Container'
 import Signup from './components/Signup';
@@ -5,9 +7,13 @@ import Header from './components/Header';
 
 const App = () => {
   return (
-    <Container>
-      <Signup />
-    </Container>
+    <Router>
+      <Container>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Container>
+    </Router>
   );
 }
 
