00a0dbbf46a9e8a0048ebf8e1b8afa1539401ef4
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-22 10:27:09 +0000

    wrap buttons with Links

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,8 +1,31 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+
 
 const LandingPage = () => {
     return (
-        <h1 className="text-center">{"Welcome to Expenses Tracker"}</h1>
+        <>
+            <h1 className="text-center mt-5">
+                {"Welcome to Expenses Tracker"}
+            </h1>
+            <Row className="mt-5">
+                <Col md={{ span: 6, offset: 3 }}>
+                    <Link to="/signup">
+                        <Button variant="success" size="lg" block>{"Sign Up"}</Button>
+                    </Link>
+                </Col>
+            </Row>
+            <Row className="mt-4">
+                <Col md={{ span: 6, offset: 3 }}>
+                    <Link to="/login">
+                        <Button variant="primary" size="lg" block>{"Log In"}</Button>
+                    </Link>
+                </Col>
+            </Row>
+        </>
     );
 };
 
