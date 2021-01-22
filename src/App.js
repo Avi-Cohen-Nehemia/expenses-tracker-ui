85386ec06d7e8dbdd648890f1697796c168320be
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-22 09:07:51 +0000

    wrap App component with container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Container from 'react-bootstrap/Container'
 import Login from './components/Login';
 import Header from './components/Header';
 
 const App = () => {
   return (
-    <>
+    <Container>
       <Login />
-    </>
+    </Container>
   );
 }
 
