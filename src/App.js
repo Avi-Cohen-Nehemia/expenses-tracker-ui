1a72b609f76caa3d18f1ef2c18439cc62dc3da1b
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-22 10:10:36 +0000

    create a basic LandingPage component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Container from 'react-bootstrap/Container'
 import Signup from './components/Signup';
-import Header from './components/Header';
 
 const App = () => {
   return (
