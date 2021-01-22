e9e03c8eed3d9b7267e442a504baadc097bc1142
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-22 09:28:23 +0000

    create a signup component and import it to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Container from 'react-bootstrap/Container'
-import Login from './components/Login';
+import Signup from './components/Signup';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <Container>
-      <Login />
+      <Signup />
     </Container>
   );
 }
