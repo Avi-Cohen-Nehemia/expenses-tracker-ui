3c659809979d6bb46682483e7bbc90a17625833c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-25 13:34:39 +0000

    limit displayed transactions to 5

--- src/components/Dashboard/TransactionsList/index.js
+++ src/components/Dashboard/TransactionsList/index.js
@@ -3,6 +3,15 @@ import ListGroup from 'react-bootstrap/ListGroup'
 
 class TransactionsList extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            transactionsToDisplay: 5,
+        }
+
+    }
 
     render() {
         return (
@@ -10,6 +19,7 @@ class TransactionsList extends Component {
                 <h4>{"Recent Transactions"}</h4>
                 <ListGroup style={{maxWidth: "400px"}}>
                     { this.props.transactions.map((transaction, index) => (
+                        index < this.state.transactionsToDisplay ?
                         <ListGroup.Item
                             className="d-flex justify-content-between"
                             variant={ transaction.type === "income" ? "success" : "danger" }
@@ -18,6 +28,7 @@ class TransactionsList extends Component {
                             <span>{transaction.category}</span>
                             <span>{transaction.amount}</span>
                         </ListGroup.Item>
+                        : null
                     )) }
                 </ListGroup>
             </>
