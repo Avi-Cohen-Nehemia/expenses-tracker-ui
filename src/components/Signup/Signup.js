193c58bba3fc6710cd7510e1345fff5e5b81a2e8
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-28 16:03:58 +0000

    give signup page same styling as landing

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
+import homeImage from "../../assets/images/home-picture2.jpg"
 
 class Signup extends Component {
 
@@ -44,89 +45,98 @@ class Signup extends Component {
         const { username, password, confirmPassword } = this.state;
 
         return(
-            <>
-                <h2 className="mt-5 text-center">{"Sign Up"}</h2>
-                <Form
-                    className="mt-5"
-                    onSubmit={this.handleSubmit}
-                >
-                    <Row>
+            <div className="landing-page">
+                <div className="mx-3" style={{ marginTop: "15%" }}>
+                    <h2 className="mt-5 text-center">{"Sign Up"}</h2>
+                    <Form
+                        className="mt-5"
+                        onSubmit={this.handleSubmit}
+                    >
+                        <Row>
+                            <Col md={{ span: 6, offset: 3 }}>
+                                <Form.Group controlId="signup-form-username">
+                                    <Form.Label>{"Username"}</Form.Label>
+                                    <Form.Control
+                                        aria-describedby="password-description"
+                                        onChange={(e) => this.handleChange(e, "username")}
+                                        required
+                                        value={username}
+                                        type="text"
+                                        style={{border: username.length >= 3 ? "2px solid green" : ""}}
+                                    />
+                                    <Form.Text
+                                        id="password-description"
+                                        muted
+                                    >
+                                        {"Username must be at least 3 characters long"}
+                                    </Form.Text>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col md={{ span: 6, offset: 3 }}>
+                                <Form.Group controlId="signup-form-password">
+                                    <Form.Label>{"Password"}</Form.Label>
+                                    <Form.Control
+                                        aria-describedby="password-description"
+                                        onChange={(e) => this.handleChange(e, "password")}
+                                        required
+                                        value={password}
+                                        type="password"
+                                        style={{border: password.length > 7 && password.length < 21 ? "2px solid green" : ""}}
+                                    />
+                                    <Form.Text id="password-description" muted>{"Password length must be between 8-20 characters"}</Form.Text>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col md={{ span: 6, offset: 3 }}>
+                                <Form.Group controlId="signup-form-confirm-password">
+                                    <Form.Label>{"Confirm Password"}</Form.Label>
+                                    <Form.Control
+                                        onChange={(e) => this.handleChange(e, "confirmPassword")}
+                                        required
+                                        value={this.state.confirmPassword}
+                                        type="password"
+                                        style={{border: confirmPassword.length > 0 && confirmPassword === password ? "2px solid green" : ""}}
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col md={{ span: 6, offset: 3 }}>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Remember me"/>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col md={{ span: 6, offset: 3 }}>
+                                <Button variant="primary" type="submit">
+                                    {"Submit"}
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+
+                    <Row className="mt-4">
                         <Col md={{ span: 6, offset: 3 }}>
-                            <Form.Group controlId="signup-form-username">
-                                <Form.Label>{"Username"}</Form.Label>
-                                <Form.Control
-                                    aria-describedby="password-description"
-                                    onChange={(e) => this.handleChange(e, "username")}
-                                    required
-                                    value={username}
-                                    type="text"
-                                    style={{border: username.length >= 3 ? "2px solid green" : ""}}
-                                />
-                                <Form.Text
-                                    id="password-description"
-                                    muted
-                                >
-                                    {"Username must be at least 3 characters long"}
-                                </Form.Text>
-                            </Form.Group>
+                            <Link to="/login"><p>{"Already have an account? Log in here"}</p></Link>
                         </Col>
                     </Row>
-
-                    <Row>
-                        <Col md={{ span: 6, offset: 3 }}>
-                            <Form.Group controlId="signup-form-password">
-                                <Form.Label>{"Password"}</Form.Label>
-                                <Form.Control
-                                    aria-describedby="password-description"
-                                    onChange={(e) => this.handleChange(e, "password")}
-                                    required
-                                    value={password}
-                                    type="password"
-                                    style={{border: password.length > 7 && password.length < 21 ? "2px solid green" : ""}}
-                                />
-                                <Form.Text id="password-description" muted>{"Password length must be between 8-20 characters"}</Form.Text>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col md={{ span: 6, offset: 3 }}>
-                            <Form.Group controlId="signup-form-confirm-password">
-                                <Form.Label>{"Confirm Password"}</Form.Label>
-                                <Form.Control
-                                    onChange={(e) => this.handleChange(e, "confirmPassword")}
-                                    required
-                                    value={this.state.confirmPassword}
-                                    type="password"
-                                    style={{border: confirmPassword.length > 0 && confirmPassword === password ? "2px solid green" : ""}}
-                                />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col md={{ span: 6, offset: 3 }}>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Remember me"/>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col md={{ span: 6, offset: 3 }}>
-                            <Button variant="primary" type="submit">
-                                {"Submit"}
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-
-                <Row className="mt-4">
-                    <Col md={{ span: 6, offset: 3 }}>
-                        <Link to="/login"><p>{"Already have an account? Log in here"}</p></Link>
-                    </Col>
-                </Row>
-            </>
+                </div>
+                <div>
+                    <img
+                        src={ homeImage }
+                        alt="person counting money"
+                        style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+                    />
+                </div>
+            </div>
         )
     };
 };
