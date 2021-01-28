0f8b0f1e0fc7abfabf82d4a478c87870b605c419
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-28 12:17:03 +0000

    add persistState to store

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from './reducer';
+import persistState from "redux-localstorage";
 import initialState from './initialState';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -8,7 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
     initialState,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk), persistState())
 );
 
-export default store
+export default store;
