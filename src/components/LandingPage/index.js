9f3025a9a2a5cdaaf64b0e74ceed8481a84b23db
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-29 12:32:40 +0000

    redirect users who are already logged in from home page to their dashboard

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,45 +1,23 @@
-import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
-import homeImage from "../../assets/images/home-picture2.jpg"
+import { connect } from "react-redux";
+import LandingPage from "./LandingPage";
+import history from "../../history";
 
-class LandingPage extends Component {
-
-    render() {
-
-        return (
-            <div className="landing-page">
-                <div className="my-auto">
-                    <h1 className="text-center">
-                        {"Welcome to Expenses Tracker"}
-                    </h1>
-                    <Row className="mt-4">
-                        <Col xs={{ span: 8, offset: 2 }} md={{ span: 6, offset: 3 }}>
-                            <Link to="/login" className="text-decoration-none">
-                                <Button variant="success" size="lg" block>{"Log In"}</Button>
-                            </Link>
-                        </Col>
-                    </Row>
-                    <Row className="mt-4">
-                        <Col xs={{ span: 8, offset: 2 }} md={{ span: 6, offset: 3 }}>
-                            <Link to="/signup" className="text-decoration-none">
-                                <Button variant="primary" size="lg" block>{"Sign Up"}</Button>
-                            </Link>
-                        </Col>
-                    </Row>
-                </div>
-                <div>
-                    <img
-                        src={ homeImage }
-                        alt="person counting money"
-                        className="cover-image"
-                    />
-                </div>
-            </div>
-        );
-    }
+// mapStateToProps is a function we use to fetch state from the
+// global state file and pass it down to a component as a prop
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.isLoggedIn,
+    };
 };
 
-export default LandingPage;
+// mapDispatchToProps is a function we use to trigger a change in state
+// and those function can be passed to the component as a prop
+const mapDispatchToProps = (dispatch) => {
+    return {
+        redirectUserToDashboard: () => {
+            history.push("/dashboard");
+        },
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
