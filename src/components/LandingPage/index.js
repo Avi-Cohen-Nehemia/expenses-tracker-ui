a5c8f5b95360189653a1f99455b45efcbe7ef6a3
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-29 12:34:30 +0000

    move user redirection logic to the component itself

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,6 +1,5 @@
 import { connect } from "react-redux";
 import LandingPage from "./LandingPage";
-import history from "../../history";
 
 // mapStateToProps is a function we use to fetch state from the
 // global state file and pass it down to a component as a prop
@@ -10,14 +9,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-// mapDispatchToProps is a function we use to trigger a change in state
-// and those function can be passed to the component as a prop
-const mapDispatchToProps = (dispatch) => {
-    return {
-        redirectUserToDashboard: () => {
-            history.push("/dashboard");
-        },
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
+export default connect(mapStateToProps, null)(LandingPage);
