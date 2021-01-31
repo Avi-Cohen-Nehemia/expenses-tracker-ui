0e4e224556d05dc8dcf5104e2185e2e2ce7eace9
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-31 12:27:29 +0000

    add logos and styling for navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,59 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import piggy from "../../assets/images/piggy.png"
+import profile from "../../assets/images/profile-avatar.png"
+import dashboard from "../../assets/images/dashboard.png"
+import logout from "../../assets/images/logout.png"
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="side-navbar">
-            <Link to="/">
-                
+            <Link
+                to="/"
+                className="side-navbar-link text-decoration-none"
+            >
+                <p className="m-0">{"Expenses Tracker"}</p>
+                <img
+                    src={ piggy }
+                    alt="piggy bank logo"
+                    style={{ height: "80px", width: "80px" }}
+                />
             </Link>
-            <Link to="/profile">
-
+            <Link
+                to="/profile"
+                className="side-navbar-link text-decoration-none"
+            >
+                <p className="m-0">{"My Profile"}</p>
+                <img
+                    className="mt-2 mb-3"
+                    src={ profile }
+                    alt="profile logo"
+                    style={{ height: "50px", width: "55px" }}
+                />
             </Link>
-            <Link to="/dashboard">
-
+            <Link
+                to="/dashboard"
+                className="side-navbar-link text-decoration-none"
+            >
+                <p className="m-0">{"Dashboard"}</p>
+                <img
+                    className="mb-1"
+                    src={ dashboard }
+                    alt="dashboard logo"
+                    style={{ height: "65px", width: "65px" }}
+                />
             </Link>
-            <Button>{"Logout"}</Button>
+            <div
+                className="side-navbar-link text-decoration-none"
+            >
+                <p className="m-0">{"Logout"}</p>
+                <img
+                    className="mt-2 mb-3 ml-2"
+                    src={ logout }
+                    alt="logout logo"
+                    style={{ height: "50px", width: "55px" }}
+                />
+            </div>
         </nav>
     );
 };
