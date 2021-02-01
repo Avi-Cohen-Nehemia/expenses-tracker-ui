af90933084fecc2b8c683cf1f22305c44c9be0ae
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-01 10:15:28 +0000

    add basic basic card styling

--- src/components/Dashboard/DashboardCard/index.js
+++ src/components/Dashboard/DashboardCard/index.js
@@ -4,7 +4,14 @@ import Card from 'react-bootstrap/Card'
 const DashboardCard = () => {
     return (
         <Card>
-
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                </Card.Text>
+            </Card.Body>
         </Card>
     );
 };
