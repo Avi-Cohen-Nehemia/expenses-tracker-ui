f79b257996048fbb6a7dbdd813c5248b0c10a11d
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-05 10:33:37 +0000

    change Navbar into a class based component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import NavbarLink from "./NavbarLink";
 import piggy from "../../assets/images/piggy.png"
 import profile from "../../assets/images/profile-avatar.png"
@@ -6,56 +6,62 @@ import dashboard from "../../assets/images/dashboard.png"
 import logout from "../../assets/images/logout.png"
 import receipt from "../../assets/images/receipt.png"
 
-const Navbar = ({ selected, handleLogout }) => {
-    return (
-        <nav className="side-navbar">
-            <div className="burger">
-                <i className="d-sm-block d-md-none fas fa-bars fa-2x text-white mr-3"/>
-            </div>
-            <NavbarLink
-                destination="/"
-                icon={ piggy }
-                text="Expenses Tracker"
-                altText="piggy bank logo"
-                styling={{height: "70px", width: "70px"}}
-            />
-            <NavbarLink
-                destination="/dashboard"
-                icon={ dashboard }
-                text="Dashboard"
-                altText="dashboard logo"
-                styling={{ height: "50px", width: "50px" }}
-                iconClasses="mb-2"
-                selected={ selected === "dashboard"}
-            />
-            <NavbarLink
-                destination="/profile"
-                icon={ profile }
-                text="Profile"
-                altText="profile logo"
-                styling={{ height: "35px", width: "40px" }}
-                iconClasses="mt-1 mb-3"
-            />
-            <NavbarLink
-                destination="/add-transaction"
-                icon={ receipt }
-                text="Add Transaction"
-                altText="add transaction logo"
-                styling={{ height: "40px", width: "45px" }}
-                iconClasses="mt-2 mb-3"
-                selected={ selected === "add-transaction"}
-            />
-            <NavbarLink
-                destination="#"
-                icon={ logout }
-                text="Logout"
-                altText="logout logo"
-                styling={{ height: "35px", width: "40px" }}
-                iconClasses="mt-2 mb-3 ml-1"
-                handleClick={ handleLogout }
-            />
-        </nav>
-    );
-};
+class Navbar extends Component {
+
+    render() {
+
+        const { selected, handleLogout } = this.props;
+
+        return (
+            <nav className="side-navbar">
+                <div className="burger">
+                    <i className="d-sm-block d-md-none fas fa-bars fa-2x text-white mr-3"/>
+                </div>
+                <NavbarLink
+                    destination="/"
+                    icon={ piggy }
+                    text="Expenses Tracker"
+                    altText="piggy bank logo"
+                    styling={{height: "70px", width: "70px"}}
+                />
+                <NavbarLink
+                    destination="/dashboard"
+                    icon={ dashboard }
+                    text="Dashboard"
+                    altText="dashboard logo"
+                    styling={{ height: "50px", width: "50px" }}
+                    iconClasses="mb-2"
+                    selected={ selected === "dashboard"}
+                />
+                <NavbarLink
+                    destination="/profile"
+                    icon={ profile }
+                    text="Profile"
+                    altText="profile logo"
+                    styling={{ height: "35px", width: "40px" }}
+                    iconClasses="mt-1 mb-3"
+                />
+                <NavbarLink
+                    destination="/add-transaction"
+                    icon={ receipt }
+                    text="Add Transaction"
+                    altText="add transaction logo"
+                    styling={{ height: "40px", width: "45px" }}
+                    iconClasses="mt-2 mb-3"
+                    selected={ selected === "add-transaction"}
+                />
+                <NavbarLink
+                    destination="#"
+                    icon={ logout }
+                    text="Logout"
+                    altText="logout logo"
+                    styling={{ height: "35px", width: "40px" }}
+                    iconClasses="mt-2 mb-3 ml-1"
+                    handleClick={ handleLogout }
+                />
+            </nav>
+        );
+    }
+}
 
 export default Navbar;
