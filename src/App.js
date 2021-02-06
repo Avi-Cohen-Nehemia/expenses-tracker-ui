5fffbe6fb5d7dbe42fd370764f4784e4c83cda3d
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-06 11:58:30 +0000

    add image and styling to 404 component and import it in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "./history"
 import "./assets/css/landing-page.css";
 import "./assets/css/navbar.css";
@@ -10,6 +10,7 @@ import "./assets/css/misc.css";
 import "./assets/css/add-transaction.css";
 import "./assets/css/profile.css";
 import "./assets/css/transaction-list.css";
+import "./assets/css/four-oh-four.css";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
@@ -17,28 +18,32 @@ import Dashboard from "./components/Dashboard";
 import AddTransaction from "./components/Dashboard/AddTransaction";
 import Profile from './components/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
+import FourOhFour from "./components/FourOhFour";
 
 const App = () => {
   return (
     <Router history={ history }>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/dashboard">
-          <ProtectedRoute Component={ Dashboard } />
-        </Route>
-        <Route exact path="/profile">
-          <ProtectedRoute Component={ Profile } />
-        </Route>
-        <Route exact path="/add-transaction">
-          <ProtectedRoute Component={ AddTransaction } />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/dashboard">
+            <ProtectedRoute Component={ Dashboard } />
+          </Route>
+          <Route exact path="/profile">
+            <ProtectedRoute Component={ Profile } />
+          </Route>
+          <Route exact path="/add-transaction">
+            <ProtectedRoute Component={ AddTransaction } />
+          </Route>
+          <FourOhFour />
+        </Switch>
     </Router>
   );
 }
--- src/components/FourOhFour/index.js
+++ src/components/FourOhFour/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import fourOfFour from "../../assets/images/fourOhFour.jpg"
+import React from "react";
+import fourOfFour from "../../assets/images/fourOhFour.png";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
 const FourOhFour = () => {
     return (
-        <>
-            <h1 className="display-3">{"Oops! We can't seem to find the page you are looking for."}</h1>
-            <img
-                src={ fourOfFour }
-                alt="404 error"
-                className="four-oh-four"
-            />
-        </>
+        <Container>
+            <h1 className="display-3 mt-5">{ "Oops!" }</h1>
+            <h2 className="display-4">{ "We can't seem to find the page you are looking for." }</h2>
+            <div className="four-oh-four">
+                <img
+                    src={ fourOfFour }
+                    alt="404 error"
+                    style={{maxWidth: "100%"}}
+                />
+            </div>
+            <Link to="/"><p>{"Navigate back to safety"}</p></Link>
+        </Container>
     );
 };
 
