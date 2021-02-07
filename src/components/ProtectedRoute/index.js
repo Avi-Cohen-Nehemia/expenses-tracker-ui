a72347204d6f44baae8425fb791186e52b06cc1c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-07 12:55:29 +0000

    remove comment

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,8 +1,6 @@
 import { connect } from "react-redux";
 import ProtectedRoute from "./ProtectedRoute";
 
-// mapStateToProps is a function we use to fetch state from the
-// global state file and pass it down to a component as a prop
 const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.isLoggedIn
