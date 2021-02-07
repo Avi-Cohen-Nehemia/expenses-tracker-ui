e8599ec80d6ac066c139a014407daf975e4c023d
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-07 12:16:58 +0000

    change pir chart into a class based component

--- src/components/Dashboard/PieChartCard/index.js
+++ src/components/Dashboard/PieChartCard/index.js
@@ -3,61 +3,66 @@ import Card from 'react-bootstrap/Card';
 import Form from "react-bootstrap/Form";
 import { PieChart } from 'react-minimal-pie-chart';
 
-const PieChartCard = ({ data }) => {
+class PieChartCard extends Component {
 
-    let colors = ["#c23127", "#c27727", "#c2b827", "#27c253", "#27c2b5", "#2772c2", "#6527c2", "#b527c2", "#751226", "#125275"];
+    render() {
 
-    let chartData = data.map((dataPoint, index) => {
-        return {
-            title: dataPoint.category,
-            value: dataPoint.amount,
-            color: colors[index]
-        };
-    });
+        let colors = ["#c23127", "#c27727", "#c2b827", "#27c253", "#27c2b5", "#2772c2", "#6527c2", "#b527c2", "#751226", "#125275"];
 
-    return (
-        <Card className="shadow pie-card">
-            <Card.Header className="pie-chart-card-header">
-                <span className="dashboard-card-header">{ "Categories Chart" }</span>
-                <div className="d-flex justify-content-start align-items-center">
-                    <Form className="mr-3">
-                        <Form.Check 
-                            type="switch"
-                            id="pie-chart-switch"
+        const { data } = this.props;
+
+        let chartData = data.map((dataPoint, index) => {
+            return {
+                title: dataPoint.category,
+                value: dataPoint.amount,
+                color: colors[index]
+            };
+        });
+
+        return (
+            <Card className="shadow pie-card">
+                <Card.Header className="pie-chart-card-header">
+                    <span className="dashboard-card-header">{ "Categories Chart" }</span>
+                    <div className="d-flex justify-content-start align-items-center">
+                        <Form className="mr-3">
+                            <Form.Check 
+                                type="switch"
+                                id="pie-chart-switch"
+                        />
+                        </Form>
+                        <i className="fas fa-chart-pie fa-lg"/>
+                    </div>
+                </Card.Header>
+                <Card.Body className="pie-chart-card-body">
+                    <div className="pie-chart-categories">
+                        {chartData.map((dataPoint, index) => (
+                            <span className="pie-chart-category-container" key={index}>
+                                <div
+                                    className="pie-chart-category-dot"
+                                    style={{ backgroundColor: dataPoint.color }}
+                                />
+                                <span>{ dataPoint.title }</span>
+                            </span>
+                        ))}
+                    </div>
+                    <PieChart
+                        animate
+                        animationDuration={ 800 }
+                        data={ chartData }
+                        label={({ dataEntry }) => `£${ dataEntry.value.toFixed(2) }`}
+                        labelPosition={ 60 }
+                        labelStyle={{
+                            color: "black",
+                            fontSize: chartData.length > 5 ? "0.4rem" : "0.5rem",
+                            fontWeight: "bold",
+                            textTransform: "capitalize"
+                        }}
+                        style={{ maxWidth: "40%" }}
                     />
-                    </Form>
-                    <i className="fas fa-chart-pie fa-lg"/>
-                </div>
-            </Card.Header>
-            <Card.Body className="pie-chart-card-body">
-                <div className="pie-chart-categories">
-                    {chartData.map((dataPoint, index) => (
-                        <span className="pie-chart-category-container" key={index}>
-                            <div
-                                className="pie-chart-category-dot"
-                                style={{ backgroundColor: dataPoint.color }}
-                            />
-                            <span>{ dataPoint.title }</span>
-                        </span>
-                    ))}
-                </div>
-                <PieChart
-                    animate
-                    animationDuration={ 800 }
-                    data={ chartData }
-                    label={({ dataEntry }) => `£${ dataEntry.value.toFixed(2) }`}
-                    labelPosition={ 60 }
-                    labelStyle={{
-                        color: "black",
-                        fontSize: chartData.length > 5 ? "0.4rem" : "0.5rem",
-                        fontWeight: "bold",
-                        textTransform: "capitalize"
-                    }}
-                    style={{ maxWidth: "40%" }}
-                />
-            </Card.Body>
-        </Card>
-    );
+                </Card.Body>
+            </Card>
+        );
+    };
 };
 
 export default PieChartCard;
