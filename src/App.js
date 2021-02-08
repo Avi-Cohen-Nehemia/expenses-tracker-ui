e4e05b239f36f3a136af26df95ec5a9a2501826e
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-08 10:59:33 +0000

    import all css files into 1 main styling file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import history from "./history"
-import "./assets/css/landing-page.css";
-import "./assets/css/navbar.css";
-import "./assets/css/dashboard.css";
-import "./assets/css/spinner.css";
-import "./assets/css/compare-card.css";
-import "./assets/css/misc.css";
-import "./assets/css/add-transaction.css";
-import "./assets/css/profile.css";
-import "./assets/css/transaction-list.css";
-import "./assets/css/four-oh-four.css";
-import "./assets/css/pie-chart.css";
+import "./assets/css/main.css";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
