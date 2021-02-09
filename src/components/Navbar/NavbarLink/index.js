2826f4fdd3f775a09c7a25f9fe485762bcc0511f
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-09 14:26:36 +0000

    fix all es-lint warnings in NavbarLink component

--- src/components/Navbar/NavbarLink/index.js
+++ src/components/Navbar/NavbarLink/index.js
@@ -1,29 +1,42 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
 const NavbarLink = ({ destination, icon, isVisible, text, altText, styling, iconClasses, selected, handleClick }) => {
     return (
         <>
             <Link
-                to={ destination }
                 className={
                     "side-navbar-link text-decoration-none d-none d-md-flex"
                     + (selected ? " font-weight-bold text-white selected-link" : "")
                     + (isVisible ? " d-flex" : "")
                 }
                 onClick={ handleClick }
+                to={ destination }
             >
                 <p className="m-0">{ text }</p>
                 <img
+                    alt={ altText }
                     className={ iconClasses }
                     src={ icon }
-                    alt={ altText }
                     style={ styling }
                 />
             </Link>
-            <hr className={"d-none d-md-block" + (isVisible ? " d-flex" : "")}/>
+            <hr className={ "d-none d-md-block" + (isVisible ? " d-flex" : "") }/>
         </>
     );
+};
+
+NavbarLink.propTypes = {
+    destination: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    isVisible: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired,
+    altText: PropTypes.string.isRequired,
+    styling: PropTypes.string,
+    iconClasses: PropTypes.string,
+    selected: PropTypes.bool,
+    handleClick: PropTypes.func
 };
 
 export default NavbarLink;
