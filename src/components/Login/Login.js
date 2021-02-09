973304f2a3b6510ad70e19e87319303633349d5b
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-09 14:22:27 +0000

    fix all es-lint warnings in Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -22,19 +23,22 @@ class Login extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-    };
+    }
 
     componentDidMount() {
-        if (this.props.isLoggedIn && this.props.accessToken) {
+
+        const { accessToken, isLoggedIn } = this.props;
+
+        if (isLoggedIn && accessToken) {
             history.push("/dashboard");
-        };
-    };
+        }
+    }
 
     handleChange(e, input) {
         let change = {};
         change[input] = e.currentTarget.value;
         this.setState(change);
-    };
+    }
 
     handleSubmit(e) {
         e.preventDefault();
@@ -50,64 +54,70 @@ class Login extends Component {
             loggingIn ? <Spinner stylingClasses="dashboard-spinner"/> :
             <div className="login-page">
                 <div className="my-auto login-form">
-                    <h2 className="text-center">{"Log In"}</h2>
+                    <h2 className="text-center">{ "Log In" }</h2>
                     <Form
                         className="mt-3"
-                        onSubmit={this.handleSubmit}
+                        onSubmit={ this.handleSubmit }
                     >
                         <Row>
-                            <Col xs={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+                            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
                                 <Form.Group controlId="login-form-username">
-                                    <Form.Label>{"Username"}</Form.Label>
+                                    <Form.Label>{ "Username" }</Form.Label>
                                     <Form.Control
-                                        onChange={(e) => this.handleChange(e, "username")}
+                                        onChange={ (e) => this.handleChange(e, "username") }
                                         required
-                                        value={username}
                                         type="text"
+                                        value={ username }
                                     />
                                 </Form.Group>
                             </Col>
                         </Row>
 
                         <Row>
-                            <Col xs={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+                            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
                                 <Form.Group controlId="login-form-password">
-                                    <Form.Label>{"Password"}</Form.Label>
+                                    <Form.Label>{ "Password" }</Form.Label>
                                     <Form.Control
-                                        onChange={(e) => this.handleChange(e, "password")}
+                                        onChange={ (e) => this.handleChange(e, "password") }
                                         required
-                                        value={password}
                                         type="password"
+                                        value={ password }
                                     />
                                 </Form.Group>
                             </Col>
                         </Row>
 
                         <Row>
-                            <Col xs={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
-                                <Button variant="primary" type="submit">
-                                    {"Submit"}
+                            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
+                                <Button type="submit" variant="primary">
+                                    { "Submit" }
                                 </Button>
                             </Col>
                         </Row>
                     </Form>
 
                     <Row className="mt-4">
-                        <Col xs={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
-                            <Link to="/signup"><p>{"Don't have an account? Sign up here"}</p></Link>
+                        <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
+                            <Link to="/signup"><p>{ "Don't have an account? Sign up here" }</p></Link>
                         </Col>
                     </Row>
                 </div>
                 <div>
                     <img
-                        src={ homeImage }
                         alt="person counting money"
                         className="cover-image"
+                        src={ homeImage }
                     />
                 </div>
             </div>
         )
-    };
+    }
+}
+
+Login.propTypes = {
+    accessToken: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    login: PropTypes.func.isRequired
 };
 
 export default Login;
