97becd4721ef2aaa3de61927e0153ab45ee92ef4
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-09 13:54:44 +0000

    delete Header component

--- src/components/Dashboard/DashboardCard/index.js
+++ src/components/Dashboard/DashboardCard/index.js
@@ -4,11 +4,11 @@ import Card from 'react-bootstrap/Card';
 
 const DashboardCard = ({ cardClass, content, title, icon }) => {
     return (
-        <Card className={"shadow " + cardClass}>
+        <Card className={ "shadow " + cardClass }>
             <Card.Header
                 className="d-flex justify-content-between align-items-center"
             >
-                <span className="dashboard-card-header">{ title }</span>  
+                <span className="dashboard-card-header">{ title }</span>
                 <i className={ icon }/>
             </Card.Header>
             <Card.Body className="d-flex justify-content-center align-items-center">
