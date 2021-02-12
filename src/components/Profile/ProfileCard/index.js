6c961e05e7ef53e65910d7fe7111640ab41ee82a
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-12 13:50:50 +0000

    edit icon displays a input to edit value when clicked

--- src/components/Profile/ProfileCard/index.js
+++ src/components/Profile/ProfileCard/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import Card from 'react-bootstrap/Card';
+import PropTypes from "prop-types";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 class ProfileCard extends Component {
 
@@ -32,10 +35,33 @@ class ProfileCard extends Component {
                     <i className={ icon }/>
                 </Card.Header>
                 <Card.Title className="profile-card-title">
-                    <i className="fas fa-pencil-alt fa-lg edit-icon"></i>
+                    <i
+                        className="fas fa-pencil-alt fa-lg edit-icon"
+                        onClick={ this.handleClick }
+                    />
                 </Card.Title>
                 <Card.Body className="d-flex justify-content-center align-items-center">
-                    <h2 className="text-center text-capitalize">{ content }</h2>
+                    { !this.state.editValue
+                        ?
+                        <h2 className="text-center text-capitalize">{ content }</h2>
+                        :
+                        <Form style={{width: "100%"}}>
+                            <Row>
+                                <Col lg={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}>
+                                    <Form.Group controlId={`edit-${content}`}>
+                                        <Form.Label>{ "Enter new..." }</Form.Label>
+                                        <Form.Control
+                                            onChange={ this.handleClick }
+                                            // onChange={ (e) => this.handleChange(e, "transactionAmount") }
+                                            required
+                                            type="text"
+                                            value={ content }
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                        </Form>
+                    }
                 </Card.Body>
             </Card>
         );
