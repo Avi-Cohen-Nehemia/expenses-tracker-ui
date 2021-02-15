0986412257942025905dfae70a9e70b5b9c5f306
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-15 17:38:25 +0000

    add missing global state and methods to wrapper file

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,20 +1,23 @@
 import { connect } from "react-redux";
 import Profile from "./Profile";
 import { getUserStats } from "../../data/actions/api";
+import { editUserDetails } from "../../data/actions/api";
 import { logout } from "../../data/actions/api";
 import history from "../../history";
 
 const mapStateToProps = (state) => {
     return {
-        username: state.username,
-        email: state.email,
+        userName: state.username,
+        userEmail: state.email,
         loaded: state.loaded,
+        submittingForm: state.submittingForm
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
         getUserStats: () => dispatch(getUserStats()),
+        editUserDetails: (property, value) => dispatch(editUserDetails(property, value)),
         logoutUser: () => {
             dispatch(logout());
             history.push("/");
