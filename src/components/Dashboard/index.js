c86bd06fca3d0ab30b7770a63c98eb89463c2775
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-16 12:22:25 +0000

    remove comments

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -4,8 +4,6 @@ import { getUserStats } from "../../data/actions/api";
 import { logout } from "../../data/actions/api";
 import history from "../../history";
 
-// mapStateToProps is a function we use to fetch state from the
-// global state file and pass it down to a component as a prop
 const mapStateToProps = (state) => {
     return {
         balanceWithCurrency: state.balanceWithCurrency,
@@ -20,8 +18,6 @@ const mapStateToProps = (state) => {
     };
 };
 
-// mapDispatchToProps is a function we use to trigger a change in state
-// and those function can be passed to the component as a prop
 const mapDispatchToProps = (dispatch) => {
     return {
         getUserStats: () => dispatch(getUserStats()),
