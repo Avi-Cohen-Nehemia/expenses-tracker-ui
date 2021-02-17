8ebbc0ef2007214eb38c0ce3b13188e04d1eaebb
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-17 10:55:00 +0000

    replace axios baseURL

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,11 @@ import axios from "axios";
 
 // return a new version of axios with useful settings applied
 export default axios.create({
-        baseURL: "http://localhost:8000/api",
+        // comment out for development
+        baseURL: "https://evening-crag-12776.herokuapp.com/",
+
+        // uncomment for development
+        // baseURL: "http://localhost:8000/api",
         headers: {
         Accept: "application/json",
     },
