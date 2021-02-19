db1a443cfeceb2625594dcc154ae6d2c77c2fe92
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-19 12:02:14 +0000

    FourOhFour test - it displays all the text correctly

--- src/components/FourOhFour/index.test.js
+++ src/components/FourOhFour/index.test.js
@@ -7,4 +7,12 @@ describe("<FourOhFour />", () => {
     it("renders without errors or warnings", () => {
         render(<FourOhFour />);
     });
+
+    it("displays all the text correctly", () => {
+        const { getByText } = render(<FourOhFour />);
+
+        expect(getByText("Oops!")).toBeInTheDocument();
+        expect(getByText("We can't seem to find the page you are looking for.")).toBeInTheDocument();
+        expect(getByText("Navigate back to safety")).toBeInTheDocument();
+    });
 });
