f60100e4ed8fd8defd80e5c1b1a03e552ef2e405
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-21 13:18:59 +0000

    Carets test - renders up AND down carets when sortBy is false

--- src/components/Dashboard/TransactionsList/Carets/index.test.js
+++ src/components/Dashboard/TransactionsList/Carets/index.test.js
@@ -12,4 +12,14 @@ describe("<Carets />", () => {
     it("renders without errors or warnings", () => {
         render(<Carets {...defaultProps}/>);
     });
+
+    it("renders up AND down caret when sortBy is false", () => {
+        render(<Carets {...defaultProps}/>);
+
+        const upCaret = document.querySelector(".caret-up");
+        const downCaret = document.querySelector(".caret-down");
+
+        expect(upCaret).toBeInTheDocument();
+        expect(downCaret).toBeInTheDocument();
+    });
 });
