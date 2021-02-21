12caf25b71016c40cb500b62164998b916785973
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-21 12:57:47 +0000

    AddTransaction test - it auto selects groceries category when expense type is selected, and paycheck when income is selected

--- src/components/AddTransaction/AddTransaction.test.js
+++ src/components/AddTransaction/AddTransaction.test.js
@@ -39,6 +39,27 @@ describe("<AddTransaction />", () => {
         expect(spinner).toBeInTheDocument();
     });
 
+    it("auto selects 'groceries' category when 'expense' type is selected, and 'paycheck' when 'income' is selected", () => {
+        render(<AddTransaction {...defaultProps}/>);
+
+        // assert category input starts as paycheck by default
+        const categoriesInput = document.querySelector("#add-transaction-category");
+        expect(categoriesInput.value).toBe("paycheck");
+
+        // select "expense" in type input
+        const typeInput = document.querySelector("#add-transaction-type");
+        fireEvent.change(typeInput, { target: { value: "expense" } })
+
+        // assert category value changed to groceries automatically
+        expect(categoriesInput.value).toBe("groceries");
+
+        // select "income" in type input
+        fireEvent.change(typeInput, { target: { value: "income" } })
+
+        // assert category value changed to groceries automatically
+        expect(categoriesInput.value).toBe("paycheck");
+    });
+
     it("renders the form with the correct initial categories options", () => {
         render(<AddTransaction {...defaultProps}/>);
         const incomeCategories = ["paycheck", "gift", "other"];
@@ -55,8 +76,8 @@ describe("<AddTransaction />", () => {
         render(<AddTransaction {...defaultProps}/>);
         const expenseCategories = ["groceries", "shopping", "rent", "bills", "entertainment", "fuel", "takeaway"];
 
-        // assert expense categories do not exist in the document initially
-        let categoriesInput = document.querySelector("#add-transaction-category");
+        // assert expense categories do NOT exist in the document initially
+        const categoriesInput = document.querySelector("#add-transaction-category");
         expenseCategories.forEach((category) => {
             const option = within(categoriesInput).queryByText(category)
             expect(option).not.toBeInTheDocument();
@@ -67,7 +88,6 @@ describe("<AddTransaction />", () => {
         fireEvent.change(typeInput, { target: { value: "expense" } })
 
         // assert expense categories exist in the document
-        categoriesInput = document.querySelector("#add-transaction-category");
         expenseCategories.forEach((category) => {
             const option = within(categoriesInput).getByText(category);
             expect(option).toBeInTheDocument();
