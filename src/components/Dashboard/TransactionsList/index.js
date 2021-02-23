fbaaebf204efdd8951f4d2ff5fac6dc58cb73615
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-02-23 09:50:26 +0000

    delete transaction take data as an argument from the component

--- src/components/Dashboard/TransactionsList/index.js
+++ src/components/Dashboard/TransactionsList/index.js
@@ -2,17 +2,9 @@ import { connect } from "react-redux";
 import TransactionList from "./TransactionList";
 import { deleteTransaction } from "../../../data/actions/api";
 
-// const mapStateToProps = (state) => {
-//     return {
-//         isLoggedIn: state.isLoggedIn,
-//         accessToken: state.accessToken,
-//         username: state.username,
-//     };
-// };
-
 const mapDispatchToProps = (dispatch) => {
     return {
-        deleteTransaction: () => dispatch(deleteTransaction())
+        deleteTransaction: (data) => dispatch(deleteTransaction(data))
     };
 };
 
