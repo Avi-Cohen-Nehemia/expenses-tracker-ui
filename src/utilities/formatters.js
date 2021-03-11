fe78fd82e28b9150f90c9218d25ee86fd8d22615
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-11 10:34:43 +0000

    wrap utillity function in if statement to prevent error when argument is null

--- src/utilities/formatters.js
+++ src/utilities/formatters.js
@@ -1,8 +1,11 @@
 export const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
+    // wrap in if statement to prevent error when argument is null
+    if (date) {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const day = date.getDate();
 
-    // add 1 to month as they are 0 indexed (e.g January displays 0)
-    return `${year}-${month + 1}-${day}`;
+        // add 1 to month as they are 0 indexed (e.g January displays 0)
+        return `${year}-${month + 1}-${day}`;
+    }
 }
