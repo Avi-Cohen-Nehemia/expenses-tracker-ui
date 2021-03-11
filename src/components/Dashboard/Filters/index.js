ff41ef7444aa0b9199a6c351e918c93ae80a845d
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-03-11 10:44:20 +0000

    rename Filters file and give it an index.js wrapper

--- src/components/Dashboard/Filters/index.js
+++ src/components/Dashboard/Filters/index.js
@@ -1,86 +1,11 @@
-import React, { Component } from "react";
-import { formatDate } from "./../../../utilities/formatters";
-// import PropTypes from 'prop-types';
-import Button from "react-bootstrap/Button";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import Form from 'react-bootstrap/Form'
+import { connect } from "react-redux";
+import Filters from "./Filters";
+import { getFilteredTransactions } from "./../../../data/actions/api";
 
-class Filters extends Component {
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getFilteredTransactions: (data) => dispatch(getFilteredTransactions(data)),
+    };
+};
 
-    constructor(props) {
-
-        super(props);
-
-        this.state = {
-            startDate: new Date(),
-            endDate: "",
-            formattedStartDate: formatDate(new Date()),
-            formattedEndDate: "",
-            filtersHaveChanged: false
-        };
-
-        this.handleDateRange = this.handleDateRange.bind(this);
-    }
-
-    componentDidUpdate() {
-        if (Date.parse(this.state.endDate) < Date.parse(this.state.startDate)) {
-            this.setState({
-                endDate: "",
-                formattedEndDate: "",
-                filtersHaveChanged: false
-            });
-        }
-    }
-
-    handleDateRange(date, type, formattedType) {
-
-        const { filtersHaveChanged } = this.state;
-
-        this.setState({
-            [type]: date,
-            [formattedType]: formatDate(date),
-            filtersHaveChanged: type === "endDate" ? true : filtersHaveChanged
-        });
-    }
-
-
-    render() {
-
-        const { endDate, startDate, filtersHaveChanged } = this.state;
-
-        return (
-            <Form className="d-flex align-items-end filters">
-                <div className="d-flex flex-column">
-                    <label className="m-0">{"Start Date"}</label>
-                    <DatePicker
-                        endDate={ endDate }
-                        onChange={ (date) => this.handleDateRange(date, "startDate", "formattedStartDate") }
-                        selected={ startDate }
-                        selectsStart
-                        startDate={ startDate }
-                    />
-                </div>
-                <div className="d-flex flex-column">
-                    <label className="m-0">{"End Date"}</label>
-                    <DatePicker
-                        endDate={ endDate }
-                        minDate={ startDate }
-                        onChange={ (date) => this.handleDateRange(date, "endDate", "formattedEndDate") }
-                        selected={ endDate }
-                        selectsEnd
-                        startDate={ startDate }
-                    />
-                </div>
-                <Button
-                    className="et-button"
-                    disabled={ !filtersHaveChanged }
-                >
-                    {"Apply"}
-                </Button>
-            </Form>
-        );
-    }
-}
-
-export default Filters;
+export default connect(null, mapDispatchToProps)(Filters);
