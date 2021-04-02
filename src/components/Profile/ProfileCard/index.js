190f986816f7b240ab5ebc7f605c3515688ebd79
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-02 16:32:25 +0100

    convert ProfileCard to a functional component

--- src/components/Profile/ProfileCard/index.js
+++ src/components/Profile/ProfileCard/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
@@ -6,72 +6,67 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 
-class ProfileCard extends Component {
+const ProfileCard = ({
+    cardClass,
+    content,
+    editingInput,
+    handleChange,
+    handleClick,
+    handleSubmit,
+    title,
+    icon,
+    inputType,
+    inputValue
+}) => {
 
-    render() {
-
-        const {
-            cardClass,
-            content,
-            editingInput,
-            handleChange,
-            handleClick,
-            handleSubmit,
-            title,
-            icon,
-            inputType,
-            inputValue
-        } = this.props;
-
-        return (
-            <Card className={ "shadow " + cardClass }>
-                <Card.Header
-                    className="d-flex justify-content-between align-items-center"
-                >
-                    <span className="dashboard-card-header">{ title }</span>
-                    <i className={ icon }/>
-                </Card.Header>
-                <Card.Title className="profile-card-title">
-                    <i
-                        className={"fas fa-lg edit-icon " + (editingInput ? "fa-times" : "fa-pencil-alt")}
-                        onClick={ handleClick }
-                    />
-                </Card.Title>
-                <Card.Body className="d-flex justify-content-center align-items-center">
-                    { !editingInput ?
-                        <h2 className="profile-card-content">{ content }</h2> :
-                        <Form
-                            noValidate
-                            onSubmit={ handleSubmit }
-                            style={{ width: "100%" }}
-                        >
-                            <Row className="align-items-center">
-                                <Col xs={{ span: 8 }}>
-                                    <Form.Group controlId={ `edit-${title}` }>
-                                        <Form.Label>{ `Enter new ${title}` }</Form.Label>
-                                        <Form.Control
-                                            onChange={ handleChange }
-                                            required
-                                            type={ inputType }
-                                            value={ inputValue }
-                                        />
-                                    </Form.Group>
-                                </Col>
-                                <Col xs={{ span: 4 }}>
-                                    <Button
-                                        className="mt-3"
-                                        type="submit"
-                                    >
-                                        {"Submit"}
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                    }
-                </Card.Body>
-            </Card>
-        );
-    }
+    return (
+        <Card className={ "shadow " + cardClass }>
+            <Card.Header
+                className="d-flex justify-content-between align-items-center"
+            >
+                <span className="dashboard-card-header">{ title }</span>
+                <i className={ icon }/>
+            </Card.Header>
+            <Card.Title className="profile-card-title">
+                <i
+                    className={"fas fa-lg edit-icon " + (editingInput ? "fa-times" : "fa-pencil-alt")}
+                    onClick={ handleClick }
+                />
+            </Card.Title>
+            <Card.Body className="d-flex justify-content-center align-items-center">
+                { !editingInput ?
+                    <h2 className="profile-card-content">{ content }</h2> :
+                    <Form
+                        noValidate
+                        onSubmit={ handleSubmit }
+                        style={{ width: "100%" }}
+                    >
+                        <Row className="align-items-center">
+                            <Col xs={{ span: 8 }}>
+                                <Form.Group controlId={ `edit-${title}` }>
+                                    <Form.Label>{ `Enter new ${title}` }</Form.Label>
+                                    <Form.Control
+                                        onChange={ handleChange }
+                                        required
+                                        type={ inputType }
+                                        value={ inputValue }
+                                    />
+                                </Form.Group>
+                            </Col>
+                            <Col xs={{ span: 4 }}>
+                                <Button
+                                    className="mt-3"
+                                    type="submit"
+                                >
+                                    {"Submit"}
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                }
+            </Card.Body>
+        </Card>
+    );
 }
 
 ProfileCard.propTypes = {
