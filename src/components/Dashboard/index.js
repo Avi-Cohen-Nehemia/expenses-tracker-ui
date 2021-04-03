c6cdbc0b9dc0d5de406c7e1846865e7bf013b320
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-03 16:01:31 +0100

    convert Dashboard to functional/hooks based component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,31 +1,133 @@
-import { connect } from "react-redux";
-import Dashboard from "./Dashboard";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import { getUserStats, logout } from "../../data/actions/api";
+import Navbar from "./../Navbar";
+import Filters from "./Filters";
+import DashboardCard from "./DashboardCard";
+import Card from 'react-bootstrap/Card';
+import Spinner from "./../Spinner";
+import PieChartCard from "./PieChartCard";
+import TransactionsList from "./TransactionsList";
 import history from "../../history";
 
-const mapStateToProps = (state) => {
-    return {
-        balanceWithCurrency: state.balanceWithCurrency,
-        totalIncome: state.totalIncome,
-        totalIncomeWithCurrency: state.totalIncomeWithCurrency,
-        totalExpense: state.totalExpense,
-        totalExpenseWithCurrency: state.totalExpenseWithCurrency,
-        totalExpenseByCategory: state.totalExpenseByCategory,
-        mostSpentOnCategory: state.mostSpentOnCategory,
-        transactions: state.transactions,
-        loaded: state.loaded,
-        submittingForm: state.submittingForm
-    };
-};
+const Dashboard = () => {
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getUserStats: () => dispatch(getUserStats()),
-        logoutUser: () => {
-            dispatch(logout());
-            history.push("/");
+    const {
+        balanceWithCurrency,
+        totalIncome,
+        totalIncomeWithCurrency,
+        totalExpense,
+        totalExpenseWithCurrency,
+        totalExpenseByCategory,
+        mostSpentOnCategory,
+        transactions,
+        loaded,
+        submittingForm,
+    } = useSelector((state) => state)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!loaded) {
+            dispatch(getUserStats());
         }
-    };
-};
+    });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+    const calculateBarWidth = (amount) => {
+
+        // make sure number is absolute
+        let absoluteAmount = Math.abs(amount);
+
+        // calculate relative % of the bars
+        let total = totalIncome + totalExpense;
+
+        return total ? `${absoluteAmount / total * 100}%` : "1%";
+    }
+
+    const logoutUser = () => {
+        dispatch(logout());
+        history.push("/");
+    }
+
+    return (
+        <div className="dashboard-grid">
+            <Navbar
+                handleLogout={ logoutUser }
+                selected="dashboard"
+            />
+            <h1 className="page-header display-3">{ "Dashboard" }</h1>
+            <Filters />
+            { loaded && !submittingForm ?
+            <>
+                <DashboardCard
+                    cardClass="balance-card"
+                    content={ balanceWithCurrency }
+                    icon="fas fa-balance-scale fa-lg"
+                    title="Balance"
+                />
+                <Card className={ "shadow comparison-card" }>
+                    <Card.Header
+                        className="d-flex justify-content-between align-items-center"
+                    >
+                        <span className="dashboard-card-header">{ "Compare" }</span>
+                        <i className={ "fas fa-chart-bar fa-lg" }/>
+                    </Card.Header>
+                    <Card.Body className="comparison-card-grid p-3">
+                            <h6 className="total-income-header m-0">{ "Total Income:" }</h6>
+                            <div className="total-income-stats">
+                                <h6 className="m-0">{ totalIncomeWithCurrency }</h6>
+                                <div className="income-bar">
+                                    <div
+                                        className="expense-bar"
+                                        style={{
+                                            width: calculateBarWidth(totalIncome),
+                                            backgroundColor: "green",
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <h6 className="total-expense-header m-0">{ "Total Expense:" }</h6>
+                            <div className="total-expense-stats">
+                                <h6 className="m-0">{ totalExpenseWithCurrency }</h6>
+                                <div className="expense-bar-container">
+                                    <div
+                                        className="expense-bar"
+                                        style={{
+                                            width: calculateBarWidth(totalExpense),
+                                            backgroundColor: "red",
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                    </Card.Body>
+                </Card>
+
+                <DashboardCard
+                    cardClass="transaction-card"
+                    content={ transactions.length ? transactions[0].amount_with_currency : "N/A" }
+                    icon="fas fa-exchange-alt fa-lg"
+                    title="Last Transaction"
+                />
+
+                <DashboardCard
+                    cardClass="category-card"
+                    content={ mostSpentOnCategory ? mostSpentOnCategory : "N/A" }
+                    icon="fas fa-search-dollar fa-lg"
+                    title="Most Spent On"
+                />
+
+                <PieChartCard
+                    data={ totalExpenseByCategory }
+                />
+
+                <TransactionsList
+                    transactions={ transactions }
+                />
+            </>
+            : <Spinner stylingClasses="new-dashboard-spinner"/>
+            }
+        </div>
+    );
+}
+
+export default Dashboard;
