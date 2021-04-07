0c9801b352982227f24d79e0429e5cb10aaed9f6
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-07 10:29:13 +0100

    convert Login to a function based component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,30 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import history from "../../history";
+
+// components
+import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { Link } from "react-router-dom";
-import homeImage from "../../assets/images/home-picture2.jpg"
-import history from "../../history";
 import Spinner from "../Spinner";
 
-class Login extends Component {
+// assets
+import homeImage from "../../assets/images/home-picture2.jpg"
 
-    constructor(props) {
 
-        super(props);
+const initialLoginState = {
+    username: "",
+    password: ""
+}
 
-        this.state = {
-            username: "",
-            password: "",
-        };
+const Login = ({ submittingForm } ) => {
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const { username, password } = state;
 
-    componentDidMount() {
+    const componentDidMount = () => {
 
         const { accessToken, isLoggedIn } = this.props;
 
@@ -33,85 +32,80 @@ class Login extends Component {
         }
     }
 
-    handleChange(e, input) {
+    const handleChange = (e, input) => {
         let value = e.currentTarget.value;
         this.setState({ [input]: value });
     }
 
-    handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
         this.props.login(this.state);
     }
 
-    render() {
-
-        const { username, password } = this.state;
-        const { submittingForm } = this.props;
-
-        return(
-            <div className="login-page">
-                { submittingForm  ? <Spinner stylingClasses="dashboard-spinner"/> :
-                    <div className="my-auto login-form">
-                        <h2 className="text-center">{ "Log In" }</h2>
-                        <Form
-                            className="mt-3"
-                            noValidate
-                            onSubmit={ this.handleSubmit }
-                        >
-                            <Row>
-                                <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
-                                    <Form.Group controlId="login-form-username">
-                                        <Form.Label>{ "Username" }</Form.Label>
-                                        <Form.Control
-                                            onChange={ (e) => this.handleChange(e, "username") }
-                                            required
-                                            type="text"
-                                            value={ username }
-                                        />
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
-                                    <Form.Group controlId="login-form-password">
-                                        <Form.Label>{ "Password" }</Form.Label>
-                                        <Form.Control
-                                            onChange={ (e) => this.handleChange(e, "password") }
-                                            required
-                                            type="password"
-                                            value={ password }
-                                        />
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
-                                    <Button type="submit" variant="primary">
-                                        { "Submit" }
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-
-                        <Row className="mt-4">
+    return(
+        <div className="login-page">
+            { submittingForm  ? <Spinner stylingClasses="dashboard-spinner"/> :
+                <div className="my-auto login-form">
+                    <h2 className="text-center">{ "Log In" }</h2>
+                    <Form
+                        className="mt-3"
+                        noValidate
+                        onSubmit={ handleSubmit }
+                    >
+                        <Row>
                             <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
-                                <Link to="/signup"><p>{ "Don't have an account? Sign up here" }</p></Link>
+                                <Form.Group controlId="login-form-username">
+                                    <Form.Label>{ "Username" }</Form.Label>
+                                    <Form.Control
+                                        onChange={ (e) => handleChange(e, "username") }
+                                        required
+                                        type="text"
+                                        value={ username }
+                                    />
+                                </Form.Group>
                             </Col>
                         </Row>
-                    </div>
-                }
-                <div>
-                    <img
-                        alt="person counting money"
-                        className="cover-image"
-                        src={ homeImage }
-                    />
+
+                        <Row>
+                            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
+                                <Form.Group controlId="login-form-password">
+                                    <Form.Label>{ "Password" }</Form.Label>
+                                    <Form.Control
+                                        onChange={ (e) => handleChange(e, "password") }
+                                        required
+                                        type="password"
+                                        value={ password }
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
+                                <Button type="submit" variant="primary">
+                                    { "Submit" }
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+
+                    <Row className="mt-4">
+                        <Col lg={{ span: 6, offset: 3 }} xs={{ span: 8, offset: 2 }}>
+                            <Link to="/signup"><p>{ "Don't have an account? Sign up here" }</p></Link>
+                        </Col>
+                    </Row>
                 </div>
+            }
+            <div>
+                <img
+                    alt="person counting money"
+                    className="cover-image"
+                    src={ homeImage }
+                />
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 Login.propTypes = {
