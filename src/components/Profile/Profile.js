bd99bb8c3e2a2fd1b403cb742954f5e0d8138b6e
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-08 10:06:57 +0100

    convert Profile to a hook based component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,106 +1,138 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import React, { useEffect, useReducer } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserStats, editUserDetails, logout } from "../../data/actions/api";
+import history from "../../history";
+
+// components
 import Spinner from "../Spinner";
 import Navbar from "../Navbar";
 import ProfileCard from "./ProfileCard";
 
-class Profile extends Component {
-
-    constructor(props) {
-
-        super(props);
-
-        this.state = {
-            name: props.userName,
-            email: props.userEmail,
-            editingInput: 0
-        }
-
-        this.handleClick = this.handleClick.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    componentDidMount() {
-        if (!this.props.loaded) {
-            this.props.getUserStats();
-        }
-    }
-
-    handleClick(num) {
-        this.setState({ editingInput: this.state.editingInput === num ? 0 : num })
-    }
-
-    handleChange(e, input) {
-        let change = {};
-        change[input] = e.currentTarget.value;
-        this.setState(change);
-    }
-
-    handleSubmit(e, input) {
-        e.preventDefault();
-        this.props.editUserDetails(input, this.state[input]);
-        this.setState({ editingInput: 0 });
-    }
-
-    render() {
-
-        const { userEmail, loaded, logoutUser, userName, submittingForm } = this.props;
-        const { email, name } = this.state;
-
-        return (
-            loaded ?
-            <div className="profile-grid">
-                <Navbar
-                    handleLogout={ logoutUser }
-                    selected="profile"
-                />
-                <h1 className="page-header display-3 p-0">{ "Profile" }</h1>
-                {
-                    submittingForm ?
-                    <Spinner stylingClasses="add-transaction-spinner"/> :
-                    <>
-                        <ProfileCard
-                            cardClass="username-card"
-                            content={ userName }
-                            editingInput={ this.state.editingInput === 1 }
-                            handleChange={ (e) => this.handleChange(e, "name") }
-                            handleClick ={ () => this.handleClick(1) }
-                            handleSubmit={ (e) => this.handleSubmit(e, "name")}
-                            icon="fas fa-user fa-lg"
-                            inputType="text"
-                            inputValue={ name }
-                            title="Username"
-                        />
-                        <ProfileCard
-                            cardClass="email-card"
-                            content={ userEmail ? userEmail : "N/A" }
-                            editingInput={ this.state.editingInput === 2 }
-                            handleChange={ (e) => this.handleChange(e, "email") }
-                            handleClick ={ () => this.handleClick(2) }
-                            handleSubmit={ (e) => this.handleSubmit(e, "email") }
-                            icon="far fa-envelope fa-lg"
-                            inputToSubmit="emailInput"
-                            inputType="email"
-                            inputValue={ email }
-                            title="Email"
-                        />
-                    </>
-                }
-            </div>
-            : <Spinner stylingClasses="add-transaction-spinner"/>
-        );
-    }
+// local state + reducer
+const initialProfileState = {
+    name: "",
+    email: "",
+    editingInput: 0
 }
 
-Profile.propTypes = {
-    editUserDetails: PropTypes.func.isRequired,
-    getUserStats: PropTypes.func.isRequired,
-    userName: PropTypes.string.isRequired,
-    userEmail: PropTypes.string.isRequired,
-    loaded: PropTypes.bool.isRequired,
-    logoutUser: PropTypes.func.isRequired,
-    submittingForm: PropTypes.bool.isRequired
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "HANDLE_INPUT_CHANGE" :
+            return {
+                ...state,
+                ...action.payload
+            };
+        default : return;
+    }
 };
+
+const Profile = () => {
+
+    const [state, dispatch] = useReducer(reducer, initialProfileState);
+    const { email, name, editingInput } = state;
+
+    // Redux hooks + global state
+    const {
+        userName,
+        userEmail,
+        loaded,
+        submittingForm
+    } = useSelector((state) => state);
+
+    const reduxDispatch = useDispatch();
+
+    useEffect(() => {
+
+        if (!loaded) {
+            reduxDispatch(getUserStats());
+        }
+
+        dispatch({
+            type: "APPLY_CHANGES",
+            payload: {
+                name: userName,
+                email: userEmail,
+            }
+        });
+    }, []);
+
+    const handleClick = (num) => {
+        dispatch({ editingInput: editingInput === num ? 0 : num })
+    }
+
+    const handleChange = (e, input) => {
+
+        let value = e.currentTarget.value;
+
+        dispatch({
+            type: "APPLY_CHANGES",
+            payload: {
+                [input]: value
+            }
+        });
+    }
+
+    const handleSubmit = (e, input) => {
+
+        e.preventDefault();
+
+        reduxDispatch(editUserDetails(input, state[input]));
+
+        dispatch({
+            type: "APPLY_CHANGES",
+            payload: {
+                editingInput: 0
+            }
+        });
+    }
+
+    const logoutUser = () => {
+        reduxDispatch(logout());
+        history.push("/");
+    }
+
+    return (
+        loaded ?
+        <div className="profile-grid">
+            <Navbar
+                handleLogout={ logoutUser }
+                selected="profile"
+            />
+            <h1 className="page-header display-3 p-0">{ "Profile" }</h1>
+            {
+                submittingForm ?
+                <Spinner stylingClasses="add-transaction-spinner"/> :
+                <>
+                    <ProfileCard
+                        cardClass="username-card"
+                        content={ userName }
+                        editingInput={ editingInput === 1 }
+                        handleChange={ (e) => handleChange(e, "name") }
+                        handleClick ={ () => handleClick(1) }
+                        handleSubmit={ (e) => handleSubmit(e, "name")}
+                        icon="fas fa-user fa-lg"
+                        inputType="text"
+                        inputValue={ name }
+                        title="Username"
+                    />
+                    <ProfileCard
+                        cardClass="email-card"
+                        content={ userEmail ? userEmail : "N/A" }
+                        editingInput={ editingInput === 2 }
+                        handleChange={ (e) => handleChange(e, "email") }
+                        handleClick ={ () => handleClick(2) }
+                        handleSubmit={ (e) => handleSubmit(e, "email") }
+                        icon="far fa-envelope fa-lg"
+                        inputToSubmit="emailInput"
+                        inputType="email"
+                        inputValue={ email }
+                        title="Email"
+                    />
+                </>
+            }
+        </div>
+        : <Spinner stylingClasses="add-transaction-spinner"/>
+    );
+}
 
 export default Profile;
